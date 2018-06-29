8d56cbc50e7a0e968ec4e78c64a5dd17eb0a2b31
Austin Malerba <austin.malerba@gmail.com>
2018-06-28 22:57:12 -0500

    missed files

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 
 import ExampleComponent from 'react-sandbox-editor'
-import {ExampleComponent2, ExampleComponent3} from 'react-sandbox-editor'
+import {Sandbox, SandboxInterpreter} from 'react-sandbox-editor'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent3 text='Modern React component module' />
+        {/*<SandboxInterpreter
+          script={`console.log('hi')`}
+          template={`<div>Hello World!</div>`}
+          stylesheet={`div {background: green;}`}
+          scriptType='js'
+          templateType='html'
+          stylesheetType='css'
+        />*/}
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
+          <Sandbox
+            classes={{
+              tabsRoot:'tabs-root',
+              tabRoot: 'tab-root-text',
+              tabSelected: 'tab-selected',
+              tabsIndicator: 'tabs-indicator',
+              playButton: 'play-button'
+            }}
+          />
+        </div>
       </div>
     )
   }
